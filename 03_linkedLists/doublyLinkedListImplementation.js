c256ff5e8b23ae317573a69bc0a3a85e2d8ba59d
vamsi-budati <budativamsikrishna@gmail.com>
2026-01-03 18:26:13 -0600

    doubly linked list

---
+++ 03_linkedLists/doublyLinkedListImplementation.js
@@ -0,0 +1,9 @@
+/*
+A doubly linked list is a more complex data structure than a singly linked list, but it offers several advantages.
+The main advantage of a doubly linked list is that it allows for efficient traversal of the list in both directions. 
+This is because each node in the list contains a pointer to the previous node and a pointer to the next node. 
+This allows for quick and easy insertion and deletion of nodes from the list, as well as efficient traversal of the list in both directions.
+*/
+
+
+
