430af3de2886e482d73839319ad202ec9a55cc72
vamsi-budati <budativamsikrishna@gmail.com>
2026-01-14 23:24:36 -0600

    graph implementation with adjacent list approach using hash table

---
+++ 06_graphs/graphsImplementation.js
@@ -0,0 +1,46 @@
+class Graph {
+    constructor() {
+        this.numberOfNodes = 0;
+        this.adjacentList = {};
+    }
+
+    addVertex(node) {
+        this.adjacentList[node] = [];
+    }
+
+    addEdge(node1, node2) {
+        //undirected graph
+        if(!this.adjacentList[node1]) {
+            return; // No vertex/node found to add edge
+        }
+        this.adjacentList[node1].push(node2);
+    }
+
+    showConnections() {
+        let connectionRepresentationStr = '';
+        for(const key in this.adjacentList) {
+            connectionRepresentationStr += `${key} -> ${this.adjacentList[key].join(' ')} \n`;
+        }
+
+        return connectionRepresentationStr;
+    }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex('0');
+myGraph.addVertex('1');
+myGraph.addVertex('2');
+myGraph.addVertex('3');
+myGraph.addVertex('4');
+myGraph.addVertex('5');
+myGraph.addVertex('6');
+myGraph.addEdge('3', '1');
+myGraph.addEdge('3', '4');
+myGraph.addEdge('4', '2');
+myGraph.addEdge('4', '5');
+myGraph.addEdge('1', '2');
+myGraph.addEdge('1', '0');
+myGraph.addEdge('0', '2');
+myGraph.addEdge('6', '5');
+
+console.log(myGraph.showConnections());
