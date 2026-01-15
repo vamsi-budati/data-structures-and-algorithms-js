82a7f6d7befaf3abae42495e75fa1f6553b5d524
vamsi-budati <budativamsikrishna@gmail.com>
2026-01-14 23:51:24 -0600

    modifies graph implementation

--- 06_graphs/graphsImplementation.js
+++ 06_graphs/graphsImplementation.js
@@ -6,14 +6,16 @@ class Graph {
 
     addVertex(node) {
         this.adjacentList[node] = [];
+        this.numberOfNodes++;
     }
 
     addEdge(node1, node2) {
         //undirected graph
-        if(!this.adjacentList[node1]) {
+        if(!this.adjacentList[node1] || !this.adjacentList[node2]) {
             return; // No vertex/node found to add edge
         }
         this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
     }
 
     showConnections() {
