8b9da969005dddbd80d5a48619227b6526ed2e86
vamsi-budati <budativamsikrishna@gmail.com>
2026-02-12 19:53:51 -0600

    insertion sort

---
+++ 08_sorting/insertionSort.js
@@ -0,0 +1,12 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+    let arrayLength = array.length;
+    for (let i = 0; i < arrayLength; i++) {
+        
+    }
+}
+insertionSort(numbers);
+
+console.log(numbers);
+
